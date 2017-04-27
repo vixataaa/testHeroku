5d8b5c4f775844cdf3814c970574067cd592f154
vixataaa <vmetodiev93@gmail.com>
2017-04-28 02:59:45 +0300

    asd

--- app.js
+++ app.js
@@ -3,21 +3,22 @@ var low = require('lowdb');
 var bodyParser = require('body-parser');
 
 app.set('port', (process.env.PORT || 5000));
-
-var app = express();
-var db = low('data/objects.json');
-
-db._.mixin(require('underscore-db'));
-
-app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
-app.use('/libs', express.static(__dirname + '/node_modules'));
-
-var objectsRouter = require('./routers/objectsRouter')(db);
-app.use('/objects', objectsRouter);
 
 
-var port = process.env.port || 3013;
+// var app = express();
+// var db = low('data/objects.json');
+
+// db._.mixin(require('underscore-db'));
+
+// app.use(bodyParser.json());
+// // app.use('/libs', express.static(__dirname + '/node_modules'));
+
+// var objectsRouter = require('./routers/objectsRouter')(db);
+// app.use('/objects', objectsRouter);
+
+
+// var port = process.env.port || 3013;
 
 // app.get('', function(req, res) {
 //     res.send('It works');
