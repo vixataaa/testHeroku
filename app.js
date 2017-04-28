db66ac04fdfdcf6280f9e8c93696e20f6e3ad4b9
vixataaa <vmetodiev93@gmail.com>
2017-04-28 03:05:10 +0300

    asd

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 
-var db = low('data/objects.json');
+var db = low(__dirname + '/data/objects.json');
 
 db._.mixin(require('underscore-db'));
 
