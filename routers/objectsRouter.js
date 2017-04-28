79055f3fdd00c4e6ba73ddf6c7047a828b1ae7f6
vixataaa <vmetodiev93@gmail.com>
2017-04-28 03:12:32 +0300

    asd

--- routers/objectsRouter.js
+++ routers/objectsRouter.js
@@ -3,12 +3,12 @@ var express = require('express');
 module.exports = function (db) {
     var router = express.Router();
 
-    router.get('/', function(req, res) {
-        res.send(db);
-    });
+    // router.get('/', function(req, res) {
+    //     res.send(db);
+    // });
 
-    router.get('hotels/:id', function(req, res) {
-        res.send(db.get('hotels').find({ 'name' : `Hotel ${req.params.id}`}))
+    router.get('/hotels/:id', function(req, res) {
+        res.send(db.get('hotels').find({ name : `Hotel ${req.params.id}`}))
     });
 
     return router;
